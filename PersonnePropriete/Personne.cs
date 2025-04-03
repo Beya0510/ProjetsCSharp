8863e68262de7ea3e49609962e6246808473e1c9
Jean-Marie BEYAMULUMBA <beyamulumbajm@gmail.com>
2025-04-03 23:38:04 +0200

    Exemple de propriété Cours 6

---
+++ PersonnePropriete/Personne.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonnePropriete;
+
+public class Personne
+{
+    // Propriété pour l'âge de Personne
+    public int Age { get; set; }
+    
+    // Constructeur pour initialiser l'âge
+    public Personne(int age)
+    {
+        Age = age;
+    }    
+    
+    // Méthode pour afficher des informations sur la personne
+    public override string ToString()
+    {
+        return $"Age: {Age}";
+    }
+}
---
+++ PersonnePropriete/Program.cs
@@ -0,0 +1,13 @@
+﻿using PersonnePropriete;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Création d'une instance Personne
+        Personne personne = new Personne(52);
+        
+        // Affichage de l'âge de la personne
+        Console.WriteLine(personne.ToString());
+    }
+}
