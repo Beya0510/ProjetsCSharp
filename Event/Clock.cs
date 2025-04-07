b378e60ad12e96934d49a18c397fd3ee13b50164
Jean-Marie BEYAMULUMBA <beyamulumbajm@gmail.com>
2025-04-07 23:34:36 +0200

    Event

---
+++ Event/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Event;
+
+    // Déclaration d'une classe dérivée de EventArgs
+    public class SecondChangedEventArgs : EventArgs
+    {
+        public int SecondsElapsed { get; }
+
+        public SecondChangedEventArgs(int secondsElapsed)
+        {
+            SecondsElapsed = secondsElapsed;
+        }
+    }
+
+    // Délégué utilisant la nouvelle classe d'arguments
+    public delegate void SecondChangedEvent(object sender, SecondChangedEventArgs e);
+
+    public class Clock
+    {
+        public event SecondChangedEvent? SecondChanged;
+        private int secondsElapsed = 0;
+
+        public void StartClock()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                secondsElapsed++;
+                OnSecondChanged(secondsElapsed);
+            }
+        }
+
+        protected virtual void OnSecondChanged(int seconds)
+        {
+            // Création d'un objet EventArgs personnalisé et invocation
+            SecondChanged?.Invoke(this, new SecondChangedEventArgs(seconds));
+        }
+    }
---
+++ Event/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            // S'abonner à l'événement
+            clock.SecondChanged += (sender, e) =>
+            {
+                Console.WriteLine($"Seconde {e.SecondsElapsed} écoulée à : {DateTime.Now:T}");
+            };
+
+            // Démarrer l'horloge
+            clock.StartClock();
+        }
+    }
+}
